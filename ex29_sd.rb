f5406a863be10139d199bc912082ff8a1fc1f5cd
PTony <jokr.01@gmail.com>
2016-01-05 17:09:52 +0100

    Exo 29 avec Study drills finis

---
+++ ex29_sd.rb
@@ -0,0 +1,11 @@
+1.What do you think the if does to the code under it?
+    It introduce a condition. If the condition is true the statement under it are executed.
+2.Why does the code under the if need to be indented two spaces?
+    for readability.
+3.What happens if it isn't indented?
+    Nothing, It work like expected, but it's just less easy to read.
+4.Can you put other boolean expressions from Exercise 27 in the if-statement? Try it.
+    See ex29_sd4_5.rb
+5.What happens if you change the initial values for people, cats, and dogs?
+    See ex29_sd4_5.rb
+    Depending the values we choose, some conditions are or aren't verified, and some texts are or aren't displayed.
