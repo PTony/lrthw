a3e66a189e359d6479e242583a5dfa1e1e2a7d46
PTony <jokr.01@gmail.com>
2016-01-05 18:10:51 +0100

    correction typos exo 31

--- ex31.rb
+++ ex31.rb
@@ -4,7 +4,7 @@ print "> "
 door = $stdin.gets.chomp
 
 if door == "1"
-    puts "There's a giant bear here eatiings a chess cake. What do you do?"
+    puts "There's a giant bear here eatings a chess cake. What do you do?"
     puts "1. Take the cake."
     puts "2. Scream at the bear."
 
@@ -12,7 +12,7 @@ if door == "1"
     bear = $stdin.gets.chomp
 
     if bear == "1"
-        puts "Thee bars eats your face off. Good job!"
+        puts "The bear eats your face off. Good job!"
     elsif bear == "2"
         puts "The bear eats your legs off. Good job!"
     else
