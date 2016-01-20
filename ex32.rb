ebc3fe9d4bf0f1a19c9307f5805ae880547c32de
PTony <jokr.01@gmail.com>
2016-01-20 18:56:06 +0100

    Fin exo 32... reste study drills

--- ex32.rb
+++ ex32.rb
@@ -24,7 +24,11 @@ change.each {|i| puts "I got #{i}" }
 elements = []
 
 # then use  the range operator to do 0 to 5 counts
-(0..5.each do |i|
+(0..5).each do |i|
     puts "adding #{i} to the list."
     # pushes the i variable on the *end* of  the list
-    elements.each {|i| puts "Element was: #{i}" }
+    elements.push(i)
+end
+
+# now we can print them out too
+elements.each {|i| puts "Element was: #{i}" }
