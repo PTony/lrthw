5306bf34ee57927cd1ee1c2f4e8ba392d19c1b19
PTony <jokr.01@gmail.com>
2016-01-23 17:37:37 +0100

    ex 34 finish -  study drills ignored

---
+++ ex34.rb
@@ -0,0 +1,33 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "The animal at 1."
+puts "The animal at 1 is the 2nd animal and is ruby."
+puts "Animal at 1 = #{animals[1]}"
+
+puts "The third (3rd) animal."
+puts "The third (3rd) animal is at 2 and is peacock."
+puts "Animal at 2 = #{animals[2]}"
+
+puts "The first (1st) animal."
+puts "The first (1st) animal is at 0 and is bear."
+puts "Animal at 0 = #{animals[0]}"
+
+puts "The animal at 3."
+puts "The animal at 3 is the 4th animal and is kangaroo."
+puts "Animal at 3 = #{animals[3]}"
+
+puts "The fifth (5th) animal."
+puts "The fifth (5th) animal is at 4 and is whale."
+puts "Animal at 4 = #{animals[4]}"
+
+puts "The animal at 2."
+puts "The animal at 2 is the 3rd animal and is peacock."
+puts "Animal at 2 = #{animals[2]}"
+
+puts "The sixth (6th) animal."
+puts "The sixth (6th) animal is at 5 and is platypus."
+puts "Animal at 5 = #{animals[5]}"
+
+puts "The animal at 4."
+puts "The animal at 4 is the 5th animal and is whale."
+puts "Animal at 4 = #{animals[4]}"
