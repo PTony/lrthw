e44e8da68de14c9228ab9773b626ccfaf2f3a65b
PTony <jokr.01@gmail.com>
2016-01-29 18:48:09 +0100

    ex36 started

---
+++ ex36.rb
@@ -0,0 +1,23 @@
+def dark_room
+end
+
+def chthulhu_room
+end
+
+def bear_room
+end
+
+def skeleton_room
+end
+
+def infinite_hole
+end
+
+def secret_room
+end
+
+def empty_room
+end
+
+def treasure_room
+end
