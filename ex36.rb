6dbbbaf261dd1b4c65607c3339f3bec033e0b9d1
PTony <jokr.01@gmail.com>
2016-02-02 17:18:47 +0100

    creation des fonctions pour chaque pièces

--- ex36.rb
+++ ex36.rb
@@ -1,23 +1,32 @@
-def dark_room
+def dark_room ()
+  puts "You are in a dark room, There is nothing here except 2 two doors"
+  puts "One on left side and one on the front side"
+  puts "which one did you choose"
+
+  puts "> "
+  choice = $stdin.gets.chomp
+  if choice.include? "front"
 end
 
-def chthulhu_room
+def chthulhu_room ()
 end
 
-def bear_room
+def bear_room ()
 end
 
-def skeleton_room
+def skeleton_room ()
 end
 
-def infinite_hole
+def infinite_hole ()
 end
 
-def secret_room
+def secret_room ()
 end
 
-def empty_room
+def empty_room ()
 end
 
-def treasure_room
+def treasure_room ()
 end
+
+dark_room()
