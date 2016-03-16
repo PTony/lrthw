107841d199245ac8e10e919ca3721836b82b5ad5
PTony <jokr.01@gmail.com>
2016-03-16 18:51:06 +0100

    ex37 - to class

--- ex37.rb
+++ ex37.rb
@@ -1,14 +1,41 @@
 BEGIN {
-  puts "This is an instruction called when the script start with BEGIN keyword"
+  puts "This is an instruction called when the script start with \"BEGIN\" keyword"
 }
 
 END {
-  puts "This is an instruction called when the script is done with END keyword"
+  puts "This is an instruction called when the script is done with \"END\" keyword"
 }
 
 alias text puts
-text "We use an alias to use puts fonction with the keywork text"
+text "We use an alias to use \"puts\" fonction with the keywork \"text\""
 
-puts "and is a logical and but" and " lower priority than &&"
+puts "\"and\" is a logical and but" and "lower priority than \"&&\""
 
 begin
+  puts "Here we are in a \"begin\" block"
+  puts "We will intentionaly generate an error to catch it"
+  file = open("/unexistant_file.txt")
+rescue
+  puts "An error was detected"
+end
+
+i = 0
+while true
+  i += 1
+  puts "Here we are in a while loop, and it's pass number #{i}"
+  puts "and we meet a \"break\" instruction"
+  break
+end
+
+case i
+when 1
+  puts "i = 1"
+else
+  puts "i != 1"
+end
+
+class X
+end
+
+def x()
+end
